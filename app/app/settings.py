80e66a0c7c255aaee2cb0e9b793a07e7cade2216
Joshua Fry <fry.developer@gmail.com>
2020-08-07 10:39:39 -0600

    booking, session, time_block apps added

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'djoser',
 
     'core',
+    'booking',
+    'session',
+    'time_block',
 ]
 
 MIDDLEWARE = [
---
+++ app/time_block/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TimeBlockConfig(AppConfig):
+    name = 'time_block'
